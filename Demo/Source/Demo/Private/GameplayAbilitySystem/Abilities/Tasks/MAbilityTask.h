f120c57454551f863ebaf27ed386acab990fbae5
unknown <liudong@ztgame.com>
2023-05-25 16:45:36 +0800

    GAS

---
+++ Demo/Source/Demo/Private/GameplayAbilitySystem/Abilities/Tasks/MAbilityTask.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "MAbilityTask.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnAbilityEnd);
+
+/**
+ * 
+ */
+UCLASS()
+class UMAbilityTask : public UAbilityTask
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(BlueprintAssignable)
+	FOnAbilityEnd OnAbilityEnd;
+};
