b1d13e71b7507af9ab83743e9f2d14a5003f38c2
unknown <liudong@ztgame.com>
2023-09-08 17:51:26 +0800

    GAS

--- Demo/Source/Demo/Private/GameplayAbilitySystem/Abilities/MGameplayAbility.h
+++ Demo/Source/Demo/Private/GameplayAbilitySystem/Abilities/MGameplayAbility.h
@@ -21,6 +21,7 @@ UENUM(BlueprintType)
 enum class EActivateFailCode : uint8
 {
 	Success			UMETA(Displayname = "成功"),
+	Immunity		UMETA(Displayname = "免疫"),
 	Cooldown		UMETA(Displayname = "冷却中"),
 	OutOfRange		UMETA(Displayname = "超出距离"),
 	ToClose			UMETA(Displayname = "太近了"),
@@ -31,6 +32,8 @@ enum class EActivateFailCode : uint8
 	InValidTarget	UMETA(Displayname = "无效的目标"),
 	NoTarget		UMETA(Displayname = "需要一个目标"),
 	NoItem			UMETA(Displayname = "道具不足"),
+	
+	CodeError		UMETA(Displayname = "内存错误")
 };
 
 /**
@@ -46,31 +49,31 @@ public:
 protected:
 
 	/** 射程*/
-	UPROPERTY(EditDefaultsOnly, Category = "ProjectSS")
+	UPROPERTY(EditDefaultsOnly, Category = "ProjectM")
 	int32 Range = 1500;
 
 	/** 法力消耗*/
-	UPROPERTY(EditDefaultsOnly, Category = "CastSpell")
+	UPROPERTY(EditDefaultsOnly, Category = "ProjectM")
 	int32 Mana = 0;
 
 	/** 怒气消耗*/
-	UPROPERTY(EditDefaultsOnly, Category = "CastSpell")
+	UPROPERTY(EditDefaultsOnly, Category = "ProjectM")
 	int32 Rage = 0;
 
 	/** 能量消耗*/
-	UPROPERTY(EditDefaultsOnly, Category = "CastSpell")
+	UPROPERTY(EditDefaultsOnly, Category = "ProjectM")
 	int32 Energy = 0;
 	
 	/** 消耗品*/
-	UPROPERTY(EditDefaultsOnly, Category = "ProjectSS")
+	UPROPERTY(EditDefaultsOnly, Category = "ProjectM")
 	TArray<int32> NeedItems;
 
 	/** 技能激活时赋予效果，结束时主动移除，配合CancelAbility()，适合技能及其效果同时存在的情况*/
-	UPROPERTY(EditDefaultsOnly, Category = "ProjectSS")
+	UPROPERTY(EditDefaultsOnly, Category = "ProjectM")
 	TArray<TSubclassOf<UGameplayEffect>> OngoingEffectsToRemoveOnEnd;
 
 	/** 技能激活时赋予效果，但结束时并不会主动移除*/
-	UPROPERTY(EditDefaultsOnly, Category = "ProjectSS")
+	UPROPERTY(EditDefaultsOnly, Category = "ProjectM")
 	TArray<TSubclassOf<UGameplayEffect>> OngoingEffectsToJustApplyOnStart;
 
 private:
